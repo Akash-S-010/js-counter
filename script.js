cb8296043394988ae20ac8b9c4e3a8f5b621cdac
Akash <akashspalloor@gmail.com>
2024-10-19 16:28:21 +0530

    js completed

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 let number = document.getElementById('counter-number');
 let min = document.getElementById('min');
 let add = document.getElementById('add');
+let limitPara = document.getElementById('limit-para');
+
 let displayNum = 0
+let limit =20
 
 add.addEventListener('click',()=>{
     displayNum++;
     if(displayNum>0){
         number.style.color = "green"
+    }
+    if(displayNum>=limit){
+        displayNum=limit;
+        limitPara.innerHTML = "Sorry the limit is 20"
+        limitPara.style.fontWeight = "800"
     }
     number.innerHTML = displayNum;
 });
@@ -17,4 +25,6 @@ min.addEventListener('click',()=>{
         number.style.color = "red"
     }
     number.innerHTML = displayNum
+    limitPara.innerHTML = ""
 })
+
